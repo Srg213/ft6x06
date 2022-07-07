ea692053b292c1429aab55973fd6e373a87bb039
Shantanu <shantanu13.srg@gmail.com>
2022-07-07 20:29:47 +0530

    constants added

---
+++ target/doc/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"ft6x06":{"doc":"","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ target/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["ft6x06"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
